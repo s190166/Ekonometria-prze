7cb5a32ad04f1c1072f1e5a09e5065f5be81d410
s190166 <s190166@student.pg.edu.pl>
2025-06-11 20:53:36 +0200

    Poprawa aktualizacji

--- Próba_morana.R
+++ Próba_morana.R
@@ -1,54 +1,28 @@
-# Bibioteki 
-install.packages(c("sf", "spdep", "tmap", "spatialreg", "tidyverse"))
-library(sf)
-library(spdep)
-library(tmap)
-library(tidyverse)
-library(dplyr)
+# 
+library(plm)
+panel <- pdata.frame(eu27_merged, index = c("ID", "rok"))
 
-# wczytanie danych z pliku shape 
-eu27 <- st_read("danemapy/EU27Official.shp")
+# Fixed Effects
+fe_model <- plm(PPP ~ Electricity.prices + log(GDP) + Stopa.bezrobocia +
+                  log(Średnie.zarobki) + Ślad.zużycia + Czas.w.unii.europejskiej,
+                data = panel, model = "within")
 
-# upewnij się, że oba są typu character
-eu27$CNTR_ID <- as.character(eu27$CNTR_ID)
-dane_panelowe$ID <- as.character(dane_panelowe$skr)
+summary(fe_model)
 
-# teraz dopiero łączymy
-eu27_merged <- left_join(eu27, dane_panelowe, by = c("CNTR_ID" = "skr"))
+# Random Effects
+re_model <- plm(PPP ~ Electricity.prices + log(GDP) + Stopa.bezrobocia +
+                  log(Średnie.zarobki) + Ślad.zużycia + Czas.w.unii.europejskiej,
+                data = panel, model = "random")
 
-# Tworzymy relacje sąsiedztwa
-neigh <- poly2nb(eu27_merged)
+summary(re_model)
 
-# Lista wag z normalizacją i obsługą brakujących sąsiadów
-weights_list <- nb2listw(neigh, style = "W", zero.policy = TRUE)
+# Reszty z modelu FE
+fe_resid <- residuals(fe_model)
+moran.test(fe_resid, weights_list, zero.policy = TRUE)
 
-# Podgląd liczby sąsiadów
-table(card(neigh))
+# Reszty z modelu RE
+re_resid <- residuals(re_model)
+moran.test(re_resid, weights_list, zero.policy = TRUE)
 
-# Test globalny Morana
-moran_global <- moran.test(eu27_merged$PPP, weights_list, zero.policy = TRUE)
-print(moran_global)
-
-# Wykres rozproszenia Morana
-moran.plot(eu27_merged$PPP, weights_list, zero.policy = TRUE)
-
-# Oblicz lokalny Moran I
-local_moran <- localmoran(eu27_merged$PPP, weights_list, zero.policy = TRUE)
-
-# Dodaj wyniki do mapy
-eu27_merged$Ii <- local_moran[, "Ii"]
-eu27_merged$Pvalue <- local_moran[, "Pr(z > 0)"]
-eu27_merged$significant <- eu27_merged$Pvalue < 0.05
-
-
-
-
-
-
-
-
-
-
-
-
-
+# test 
+phtest(fe_model, re_model)
